9e5af9677514bcb5d4b13112a8ea02bf2ce10a06
dacnhan913 <157615172+dacnhan913@users.noreply.github.com>
2024-03-18 14:51:31 +0700

    Add files via upload

---
+++ Calculator/JavaScript/CalJS.js
@@ -0,0 +1,50 @@
+function dis(result) {
+    document.getElementById("div-print").value += result;
+
+}
+
+function myFunction(event) {
+    if (event.key == '0' || event.key == '1'
+        || event.key == '2' || event.key == '3'
+        || event.key == '4' || event.key == '5'
+        || event.key == '6' || event.key == '7'
+        || event.key == '8' || event.key == '9'
+        || event.key == '+' || event.key == '-'
+        || event.key == '*' || event.key == '/') {
+        document.getElementById("div-print").value += event.key;
+    }
+
+}
+
+
+var cal = document.getElementById("div-print");
+cal.onkeyup = function (event) {
+    if (event.keyCode === 13) {
+        console.log("Enter");
+        let x = document.getElementById("div-print").value;
+        console.log(x);
+        solve();
+    }
+}
+
+// Function that evaluates the digit and return result 
+function solve() {
+    let x = document.getElementById("div-print").value;
+    if (x == "" || (('A' <= x && x <= 'Z') || ('a' <= x && x <= 'z'))) {
+        clr();
+    }
+    else {
+        let y = eval(x);
+        document.getElementById("div-print").value = y;
+    }
+
+}
+
+// Function that clear the display 
+function clr() {
+    document.getElementById("div-print").value = "";
+}
+function uclr() {
+    let res = document.getElementById("div-print").value;
+    document.getElementById("div-print").value = res.substring(0, res.length - 1);
+}
